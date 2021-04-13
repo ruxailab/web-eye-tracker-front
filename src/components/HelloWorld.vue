9c922a84b9abdbd4c4e7b1c08089a3c5258f36bb
Karine <karine.pistili@gmail.com>
2021-04-13 09:51:56 -0300

    initial commit: added project base

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+   <h1>Hello World</h1>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+  }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <hello-world/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  components: { HelloWorld },
+  name: 'Home',
+}
+</script>
