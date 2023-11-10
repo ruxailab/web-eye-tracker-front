c71cfe7ab77343761da5445287943a564a0e5acb
Natanco <nata.oliveira2004@gmail.com>
2023-11-10 11:44:23 -0300

    feat: random points on screen

--- src/views/PostCalibration.vue
+++ src/views/PostCalibration.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
-        <div>{{ fixedTrainData }}</div>
-        <div>{{ predictTrainData }}</div>
-        <div>{{ prediction }}</div>
+        <canvas ref="canvas" />
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            xValues: [],
+            yValues: [],
+        };
+    },
+    mounted() {
+        this.draw();
+        setInterval(this.fillArrays, 1000);
+    },
     computed: {
         fixedTrainData() {
             return this.$store.state.predict.fixedTrainData
@@ -21,6 +29,35 @@ export default {
     },
     created() {
         console.log(this.$store.state.predict)
+    },
+    methods: {
+        fillArrays() {
+            this.xValues.push(Math.floor(Math.random() * 900));
+            this.yValues.push(Math.floor(Math.random() * 900));
+            this.draw();
+        },
+        draw() {
+            const canvas = this.$refs.canvas;
+            const ctx = canvas.getContext('2d');
+
+            const screenWidth = window.innerWidth;
+            const screenHeight = window.innerHeight;
+
+            canvas.width = screenWidth;
+            canvas.height = screenHeight;
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            for (let i = 0; i < this.xValues.length; i++) {
+                const x = this.xValues[i];
+                const y = screenHeight - this.yValues[i];
+
+                ctx.beginPath();
+                ctx.arc(x, y, 5, 0, 2 * Math.PI);
+                ctx.fillStyle = 'blue';
+                ctx.fill();
+            }
+        }
     },
 }
 </script>
