ca80c9b955bf2a81c68a8ba0d8918e7b2b3302ea
Natanco <nata.oliveira2004@gmail.com>
2023-11-14 16:37:26 -0300

    feat: radius visualization

--- src/components/calibConfig/Radius.vue
+++ src/components/calibConfig/Radius.vue
@@ -1,10 +1,10 @@
 <template>
-    <v-row>
-        <v-col cols="8">
-            <Slider :value="radius" :min="Number(20)" :max="Number(60)" label="radius" @input="updateRadius" />
+    <v-row class="d-flex align-center">
+        <v-col cols="10">
+            <Slider :value="radius" :min="Number(10)" :max="Number(35)" label="radius" @input="updateRadius" />
         </v-col>
-        <v-col cols="4" style="max-width: 100%; max-height: 100%; display: flex; align-items: stretch;">
-            <canvas id="myCanvas" style="width: 100%; height: 100%;"></canvas>
+        <v-col cols="2" style="max-width: 100%; max-height: 100%; display: flex; align-items: stretch;">
+            <canvas id="radCanvas" style="width: 100%; height: 100%;"></canvas>
         </v-col>
     </v-row>
 </template>
@@ -25,6 +25,31 @@ export default {
         updateRadius(value) {
             this.$store.commit('setRadius', value);
         },
+        drawBall(radius) {
+            const canvas = document.getElementById("radCanvas");
+            const ctx = canvas.getContext("2d");
+            const centerX = canvas.width / 2;
+            const centerY = canvas.height / 2;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.beginPath();
+            ctx.arc(centerX, centerY, radius*2, 0, 2 * Math.PI);
+            ctx.fillStyle = "black";
+            ctx.fill();
+            ctx.closePath();
+            ctx.beginPath();
+            ctx.arc(centerX, centerY, (radius/3)*2, 0, 2 * Math.PI);
+            ctx.fillStyle = "red";
+            ctx.fill();
+            ctx.closePath();
+        },
+    },
+    watch: {
+        radius(newRadius) {
+            this.drawBall(newRadius);
+        },
+    },
+    mounted() {
+        this.drawBall(this.radius)
     },
 };
 </script>
