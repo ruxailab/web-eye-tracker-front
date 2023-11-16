89ba22225385793eba7f2a785a5f92b8d4780d46
Natanco <nata.oliveira2004@gmail.com>
2023-11-16 10:22:08 -0300

    refactor: new component modularization

---
+++ src/components/calibConfig/GeneralConfigCard.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-container>
+        <v-col>
+            <v-card outlined>
+                <v-card-title>
+                    general configuration
+                </v-card-title>
+                <div class="custom-outline">
+                    <Slider :value="pointNumber" :min="Number(1)" :max="Number(9)" label="Point Number"
+                        @input="updatePointNumber" />
+                </div>
+                <div class="custom-outline">
+                    <Slider :value="samplePerPoint" label="Sample Per Point" @input="updateSamplePerPoint" />
+                </div>
+                <div class="custom-outline">
+                    <Radius />
+                </div>
+                <div class="custom-outline">
+                    <Offset />
+                </div>
+            </v-card>
+        </v-col>
+    </v-container>
+</template>
+  
+<script>
+import Slider from "@/components/Slider.vue";
+import Radius from "@/components/calibConfig/Radius"
+import Offset from "@/components/calibConfig/Offset.vue";
+
+export default {
+    components: {
+        Radius,
+        Offset,
+        Slider
+    },
+    computed: {
+        samplePerPoint() {
+            return Number(this.$store.state.calibration.samplePerPoint ?? 0);
+        },
+        pointNumber() {
+            return Number(this.$store.state.calibration.pointNumber ?? 0);
+        }
+    },
+    methods: {
+        updateSamplePerPoint(value) {
+            this.$store.commit('setSamplePerPoint', value);
+        },
+        updatePointNumber(value) {
+            this.$store.commit('setPointNumber', value);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.custom-outline {
+    border: 1px solid #000;
+    padding: 10px;
+    border-radius: 5px;
+    margin: 10px;
+}
+</style>
--- src/views/CalibrationConfig.vue
+++ src/views/CalibrationConfig.vue
@@ -1,68 +1,18 @@
 <template>
     <div>
         <Toolbar />
-        <v-container>
-            <v-col>
-                <v-card outlined>
-                    <v-card-title>
-                        general configuration
-                    </v-card-title>
-                    <div class="custom-outline">
-                        <Slider :value="pointNumber" :min="Number(1)" :max="Number(9)" label="Point Number"
-                            @input="updatePointNumber" />
-                    </div>
-                    <div class="custom-outline">
-                        <Slider :value="samplePerPoint" label="Sample Per Point" @input="updateSamplePerPoint" />
-                    </div>
-                    <div class="custom-outline">
-                        <Radius />
-                    </div>
-                    <div class="custom-outline">
-                        <Offset />
-                    </div>
-                </v-card>
-            </v-col>
-        </v-container>
+        <GeneralConfigCard />
     </div>
 </template>
   
 <script>
 import Toolbar from "@/components/Toolbar.vue";
-import Slider from "@/components/Slider.vue";
-import Radius from "@/components/calibConfig/Radius"
-import Offset from "@/components/calibConfig/Offset.vue";
+import GeneralConfigCard from "../components/calibConfig/GeneralConfigCard.vue";
 
 export default {
     components: {
         Toolbar,
-        Radius,
-        Offset,
-        Slider,
-    },
-    computed: {
-        samplePerPoint() {
-            return Number(this.$store.state.calibration.samplePerPoint ?? 0);
-        },
-        pointNumber() {
-            return Number(this.$store.state.calibration.pointNumber ?? 0);
-        }
-    },
-    methods: {
-        updateSamplePerPoint(value) {
-            this.$store.commit('setSamplePerPoint', value);
-        },
-        updatePointNumber(value) {
-            this.$store.commit('setPointNumber', value);
-        },
+        GeneralConfigCard
     },
 };
 </script>
-
-<style scoped>
-.custom-outline {
-    border: 1px solid #000;
-    padding: 10px;
-    border-radius: 5px;
-    margin: 10px;
-}
-</style>
