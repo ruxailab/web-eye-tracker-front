f682e6dc25594bdc42180a4e131bf4f9dc4d8646
Dishanth Arya <dishantharya04@gmail.com>
2025-03-17 17:10:16 +0530

    Error pop up when API is not running

--- src/views/CalibrationCard.vue
+++ src/views/CalibrationCard.vue
@@ -16,15 +16,15 @@
 <template>
   <div id="box">
     <Toolbar />
-    <v-container class="mt-12">
-      <v-row justify="center" align="start">
-        <v-col cols="12" lg="5" md="5">
-          <v-card outlined>
-            <v-card-title>
+    <v-container class="d-flex justify-center align-center fill-height">
+      <v-row justify="center" align="center">
+        <v-col cols="12" lg="5" md="6" sm="8">
+          <v-card outlined class="mx-auto" max-width="500">
+            <v-card-title class="text-center">
               <v-icon left>mdi-information</v-icon>
               Calibration System
             </v-card-title>
-            <p class="mx-4">
+            <v-card-text class="text-center">
               The calibration works by associating the position of your eyes'
               iris with the coordinates of the circle on the screen.
               <br /><br />
@@ -35,29 +35,65 @@
               Your task during the calibration is to follow the circles with
               your eyes. Try your best to not look away and to keep following.
               Blinking is fine.
-            </p>
-            <v-card-actions class="px-4 mt-12 mb-4">
-              <v-btn block outlined color="green" @click="goToCameraConfig()">Start
-                Calibration</v-btn>
+            </v-card-text>
+            <v-card-actions class="d-flex justify-center">
+              <v-btn outlined color="green" @click="startCalibration">
+                Start Calibration
+              </v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
     </v-container>
+
+    <!-- Popup Dialog -->
+    <v-dialog v-model="showError" persistent max-width="400" content-class="dialog-center">
+      <v-card>
+        <v-card-title class="red--text text-center">
+          API Error
+        </v-card-title>
+        <v-card-text class="text-center">
+          Unable to start calibration. The API is not running.
+        </v-card-text>
+        <v-card-actions class="d-flex justify-center">
+          <v-btn color="red" text @click="showError = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import Toolbar from "@/components/general/Toolbar.vue";
 
 export default {
   components: {
     Toolbar,
   },
+  data() {
+    return {
+      showError: false,
+    };
+  },
   methods: {
-    goToCameraConfig() {
-      this.$router.push("/calibration/configuration");
+    async startCalibration() {
+      const API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
+      try {
+        await axios.get(`${API_BASE_URL}/api/health`);
+        this.$router.push("/calibration/configuration");
+      } catch (error) {
+        this.showError = true;
+      }
     },
   },
 };
 </script>
+
+<style>
+.dialog-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
